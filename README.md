c1b5b8de5588a89973b7b37d885c31b1879df67b
fagner henrique flores <henriquefagner@hotmail.com>
2020-03-04 14:06:30 -0600

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # using-if-else-statements
-here you can see that an if ... else statement allows you to provide two sets of code: one set if the condition evaluates true and the other set if the condition is false 
+
+here you can see that an if ... else statement allows you to provide two sets of code: one set if the condition evaluates true and the other set if the condition is false
+more info at the file.js
---
+++ using-if-else-statment/file.js
@@ -0,0 +1,90 @@
+var score = 50; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer");
+el.textContent = msg;
+
+var score = 100; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer2");
+el.textContent = msg;
+
+var score = 70; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer3");
+el.textContent = msg;
+
+var score = 76; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer4");
+el.textContent = msg;
+
+var score = 74; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer5");
+el.textContent = msg;
+
+var score = 79; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer6");
+el.textContent = msg;
+
+var score = 79; //pass mark
+var pass = 75; //current score
+var msg; //message
+
+//select message to write based on score
+if (score >= pass) {
+  msg = "Congratulations, you passed!";
+} else {
+  msg = "have another go!";
+}
+var el = document.getElementById("answer7");
+el.textContent = msg;
